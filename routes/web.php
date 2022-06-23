f59dc2989e0abd77fb84a51119a38e5bade8e606
Roman <romaaverkin@gmail.com>
2022-06-23 11:21:19 +0700

    Группировка маршрутов

--- routes/web.php
+++ routes/web.php
@@ -17,42 +17,11 @@ Route::get('/', function () {
     return 'главная страница сайта';
 });
 
-Route::get('dir/test', function () {
-    return 'это тестовая страница в dir';
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'all';
+    });
+    Route::get('/user/{id}', function ($id) {
+        return $id;
+    });
 });
-
-Route::get('/post/{id}', function ($id) {
-    return 'пост ' . $id;
-})->where('id', '[0-9]+');
-
-//Route::get('user/{surname}/{name}', function ($surname, $name) {
-//    return "Имя: $surname, Фамилия: $name";
-//});
-
-//Route::get('/posts/page/{page?}', function ($page = 1) {
-//    return 'страница номер ' . $page;
-//});
-
-//Route::get('/city/{city?}', function ($city = "minsk") {
-//    return "Введено $city";
-//});
-
-//Route::get('user/{id}', function ($id) {
-//    return 'пост ' . $id;
-//})->where('id', '[0-9]+');
-
-//Route::get('user/{id}/{name}', function ($id, $name) {
-//    return 'пользователь ' . $id . ' имя ' . $name;
-//})->where('id', '[0-9]+')->where('name', '[a-z]{2,}');
-
-Route::get('posts/{date}', function ($date) {
-    return 'Дата равна: ' . $date;
-})->where('date', '20[0-9][0-9]+-0[0-9]|1[0-2]');
-
-Route::get('user/{id}', function ($id) {
-    return 'Маршрут равен ' . $id;
-})->whereNumber('id');
-
-Route::get('city/{name}', function ($name) {
-    return 'имя ' . $name;
-})->whereAlpha('name');
