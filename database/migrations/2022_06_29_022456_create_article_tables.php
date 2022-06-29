d2ecd2812cfbce3e622438245a13be1bf41c17d8
Roman <romaaverkin@gmail.com>
2022-06-29 09:45:01 +0700

    Изучаю миграции

--- database/migrations/2022_06_29_022456_create_article_tables.php
+++ database/migrations/2022_06_29_022456_create_article_tables.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article_tables', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->date('birthday');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('header', 255);
+            $table->text('text');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article_tables');
     }
 };
