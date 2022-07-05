f3fbd78d7d25d361fd038bb8bd67fcd26578dc05
Roman <romaaverkin@gmail.com>
2022-07-05 12:43:00 +0700

    Порядок полей в миграциях Laravel

--- database/migrations/2022_07_05_042614_change_users_table.php
+++ database/migrations/2022_07_05_042614_change_users_table.php
@@ -13,8 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('name', 'first_name');
-            $table->renameColumn('surname', 'second_name');
+            $table->string('sex')->after('id')->change();
         });
     }
 
