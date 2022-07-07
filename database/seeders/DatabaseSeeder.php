cde8595d79ec53896f4e9aff19234837dae78053
Roman <romaaverkin@gmail.com>
2022-07-07 15:44:31 +0700

    Генерация случайных строк в сидерах в Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,38 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            [
+        for ($i = 0; $i <= 10; $i++) {
+            DB::table('posts')->insert([
                 'created_at' => Carbon::now()->toDateTimeString(),
                 'updated_at' => Carbon::now()->toDateTimeString(),
-                'title' => 'title 1',
-                'slug' => 'post-1',
-                'text' => 'text text text 1',
-            ],
-            [
-                'created_at' => Carbon::now()->toDateTimeString(),
-                'updated_at' => Carbon::now()->toDateTimeString(),
-                'title' => 'title 2',
-                'slug' => 'post-2',
-                'text' => 'text text text 2',
-            ],
-            [
-                'created_at' => Carbon::now()->toDateTimeString(),
-                'updated_at' => Carbon::now()->toDateTimeString(),
-                'title' => 'title 3',
-                'slug' => 'post-3',
-                'text' => 'text text text 3',
-            ],
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Роман',
-            'surname' => 'Аверкин',
-            'birthday' => '1979-01-22',
-            'email' => 'romaaverkin@gmail.com',
-            'password' => Carbon::now()->toDateTimeString(),
-            'created_at' => Carbon::now()->toDateTimeString(),
-            'updated_at' => Carbon::now()->toDateTimeString(),
-        ]);
+                'title' => Str::random(10),
+                'slug' => Str::random(10),
+                'text' => Str::random(10),
+            ]);
+        }
     }
 }
