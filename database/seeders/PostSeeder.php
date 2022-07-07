1dd00caa391fb9804a65983cffbd6e8f41472863
Roman <romaaverkin@gmail.com>
2022-07-07 16:24:00 +0700

    В работе

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
